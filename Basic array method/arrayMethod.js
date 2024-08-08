5fa93d2e833d184448a74012ccf0a49ef336daaa
karan7594 <karanprajapat723@gmail.com>
2024-08-08 10:55:10 +0530

    array  method push, pop , concat and etc.

---
+++ Basic array method/arrayMethod.js
@@ -0,0 +1,21 @@
+let arr = ["ram ", "shyam ", "mohan", "dinesh"];
+
+// console.log(arr.length); //print the length
+
+// console.log(arr.join(" * "));   //join the element between the arr elements.
+
+// console.log(arr.pop(3));
+// console.log(arr);          // pop the third element in arr.
+
+// console.log(arr.push("karan"));   //it define the length of arr.
+// console.log(arr);                //print the arr
+
+// console.log(arr.shift());  //The shift() method returns the value that was "shifted out":
+// console.log(arr);
+
+// console.log(arr.unshift("karan"));  //length after the unshift
+// console.log(arr);        //print arr
+
+let array = ["sita", "priya"];
+let newarr = arr.concat(array);
+console.log(newarr);            //add the two array
