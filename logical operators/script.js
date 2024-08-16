0588c57e4bcaf34916357855ba1a21fe61147b63
karan7594 <karanprajapat723@gmail.com>
2024-08-16 14:53:51 +0530

    logical and or operator

---
+++ logical operators/script.js
@@ -0,0 +1,12 @@
+// Logical operator in js
+
+let x = 93;
+let y = 88;
+let z = 78;
+if (x >= y && y >= z) {
+  console.log("the and conditon is true");             //true
+} else if (x >= y || y >= z) {
+  console.log("or conditon is true");
+} else {
+  console.log("does not condion is and or .");
+}
