98e7e7d048a73ca0e695ab57002fc8b90f1f56e6
karan7594 <karanprajapat723@gmail.com>
2024-08-16 14:26:18 +0530

    if else conditions

---
+++ if else/condition.js
@@ -0,0 +1,19 @@
+//if else conditons
+
+// let age = 17;
+// if (age >= 18) {
+//   console.log("you can search for driving lience 🚗");
+// } else {
+//   let leftMyAge = 18 - age;
+//   console.log(`you can search another ${leftMyAge} year for driving lience 🚗`);
+// }
+
+// age check for the drive car
+let a = 60;
+if (a > 60) {
+  console.log("you can not drive car  ");
+} else if (a >= 18) {
+  console.log("you can drive");
+} else {
+  console.log("you can not drive car");
+}
