70edd17bb1842d994ecda0b42474ddd1f71fe104
karan7594 <karanprajapat723@gmail.com>
2024-08-16 16:42:07 +0530

    if else conditon add

--- if else/condition.js
+++ if else/condition.js
@@ -9,11 +9,25 @@
 // }
 
 // age check for the drive car
-let a = 60;
-if (a > 60) {
-  console.log("you can not drive car  ");
-} else if (a >= 18) {
-  console.log("you can drive");
-} else {
-  console.log("you can not drive car");
-}
+// let a = 60;
+// if (a > 60) {
+//   console.log("you can not drive car  ");
+// } else if (a >= 18) {
+//   console.log("you can drive");
+// } else {
+//   console.log("you can not drive car");
+// }
+
+// let a = 60;
+// a > 60
+//   ? console.log("you can not drive car  ")
+//   : a >= 18                                       //if else conditions
+//   ? console.log("you can drive")
+//   : console.log("you can not drive car");
+
+
+
+
+let age = 235;
+const check = age >= 18 ? "kjfsd" : "jsa";
+console.log(check);
