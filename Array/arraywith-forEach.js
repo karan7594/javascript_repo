7abd370baeb8b39f1b7ea80a03b916a0f803c98f
karan7594 <karanprajapat723@gmail.com>
2024-08-17 13:30:43 +0530

    commit code

--- Array/arraywith-forEach.js
+++ Array/arraywith-forEach.js
@@ -15,8 +15,21 @@
 
 // or
 
-let arrvalue = [1, 23, 24, 2, 42, 23, 2, 7];
-let squarevalue = (val) => {
-  console.log(val ** 2);
-};
-arrvalue.forEach(squarevalue);
+// let arrvalue = [1, 23, 24, 2, 42, 23, 2, 7];
+// let squarevalue = (val) => {
+//   console.log(val ** 2);
+// };
+// arrvalue.forEach(squarevalue);
+
+// [1, 2, 432, 235, 32].forEach(function (value) {
+//   console.log(value);       //normal function
+// });
+
+[1, 2, 432, 235, 32].forEach((value) => {
+  console.log(value);
+}); //with arrow function
+
+const arr = [12, 24, 24, 2, 42, 22].map((value) => {
+  return value;
+});
+console.log(arr);
